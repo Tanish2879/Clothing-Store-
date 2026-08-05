df4a5b7a7d8b28ed135c13337ac3ddda12a14de3
Tanish mattoo <tanish.mattoo@axeno.co>
2026-08-05 13:41:46 +0530

    ok

--- js/products.js
+++ js/products.js
@@ -1,144 +1,3 @@
-
-// const apiURL = "https://dummyjson.com/products/category/mens-shirts";
-
-
-
-// const productContainer =
-// document.querySelector(".product-grid");
-
-
-
-// // Array that will store all products
-
-// let products = [];
-
-
-
-// // GET PRODUCTS FROM API
-
-// async function getProducts(){
-
-
-//     // Send request to API
-
-//     const response = await fetch(apiURL);
-
-//     // Convert response into JSON
-
-//     const data = await response.json();
-//     // Store products
-
-//     products = data.products;
-//     // Display them
-
-//     displayProducts(products);
-
-// }
-
-
-
-// // DISPLAY PRODUCTS
-
-
-// function displayProducts(productList){
-
-
-//     // Remove previous products
-
-//     productContainer.innerHTML = "";
-
-
-
-//     productList.forEach(function(product){
-
-
-//         productContainer.innerHTML += `
-
-//         <article class="product-card">
-
-//             <img src="${product.thumbnail}" alt="${product.title}" width="200" height="200">
-
-//             <h3>${product.title}</h3>
-
-//             <p>${product.description}</p>
-
-//             <span class="product-price">
-
-//                 ₹${product.price}
-
-//             </span>
-
-//             <a href="#" class="btn">
-
-//                 Buy Now
-
-//             </a>
-
-//         </article>
-
-//         `;
-
-//     });
-
-// }
-
-
-
-//  //  START PROGRAM
-
-// getProducts();
-
-
-// // DARK / LIGHT THEME
-
-
-// // Get the button
-
-// const themeButton = document.getElementById("themeButton");
-
-// // Check if dark mode was saved
-
-// let savedTheme = localStorage.getItem("theme");
-
-// // If dark mode was previously selected
-
-// if(savedTheme === "dark"){
-
-//     document.body.classList.add("dark");
-
-//     themeButton.innerHTML = "Light Mode";
-
-// }
-
-// // Button click
-
-// themeButton.addEventListener("click", function(){
-
-//     // Toggle dark class
-
-//     document.body.classList.toggle("dark");
-
-//     // Check current mode
-
-//     if(document.body.classList.contains("dark")){
-
-//         localStorage.setItem("theme","dark");
-
-//         themeButton.innerHTML = "Dark Mode";
-
-//     }
-
-//     else{
-
-//         localStorage.setItem("theme","light");
-
-//         themeButton.innerHTML = "Dark Mode";
-
-//     }
-
-// });
-
-
 const apiURL = "https://dummyjson.com/products/category/mens-shirts";
 
 
@@ -174,6 +33,11 @@ async function getProducts(){
     displayProducts(products);
 
 }
+
+
+
+
+
 
 
 
